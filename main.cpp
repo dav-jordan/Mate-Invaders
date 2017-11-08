036ffc4405d008cbd0682a22de607bffdc155028
Davis Ridley Jordan <davisridleyjordan@gmail.com>
2017-11-08 13:02:48 -0500

    Added gamespace array and successfully initialized it

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
+
+#define WIDTH atoi(getenv("COLUMNS"))
+#define HEIGHT 100
 int main()
 {
-	cout << "This is a test. It worked!" << endl;
+	//to indicate the file has been excecuted
+	cout << "File successfully ran!" << endl;
+	//2d array to represent positions on the terminal screen
+	char gamespace[WIDTH][HEIGHT];
+	
+	//initialize the gamespace array
+	for(int i = 0; i < HEIGHT; i++)
+	{
+		for(int k = 0; k < WIDTH-1; k++)
+			gamespace[k][i] = ' ';
+		gamespace[WIDTH-1][i] = '\n';
+	}
+	
+	//**TESTING ONLY**
+	//print initialized array
+	for(int i = 0; i < HEIGHT; i++)
+		for(int k = 0; k < WIDTH; k++)
+			cout << gamespace[k][i];
+
+	exit(1);
+	
 }
