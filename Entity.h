d8b89f9f2cfcd669b2044b0ea12306c990d07b01
Davis Jordan <davisridleyjordan@gmail.com>
2017-11-09 10:56:07 -0500

    Added .h file for the Entity class, which will be the super class for the Player Ship, Enemy ships, and Barriers

---
+++ Entity.h
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+//The entity class is the super class of all entities in the game, 
+//which includes the ship, the enemy ships, and the barriers
+class Entity
+{
+
+//every entity has an x and y coordinate
+//only entity that won't be moveable are barriers
+//isAi used for determining whether or not the ship is the player when moving through list of entities
+private:
+	int x;
+	int y;
+	bool moveable;
+	bool ai;
+
+public:
+	//constructors
+	Entity();
+	Entity(int x, int y);
+	Entity(int x, int y, bool moveable, bool ai);
+	//destructor
+	~Entity();
+	
+	//moving functions
+	//random used for enemies which will move randomly
+	void moveUp();
+	void moveDown();
+	void moveRandom();
+
+	//basic accessors
+	int xPos();
+	int yPos();
+	bool isMoveable();
+	bool isAi();	
+};
