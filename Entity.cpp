3a690c31316813e373e00f81d2e62996dc4f8efa
Davis Jordan <davisridleyjordan@gmail.com>
2017-11-09 11:14:43 -0500

    Updated Changelog in README

--- Entity.cpp
+++ Entity.cpp
@@ -2,11 +2,29 @@
 #include "Entity.h"
 using namespace std;
 
-//default constructor for entity
+//constructors used for entity and its subclasses
+//@param	x-x coord, y-y coord, moveable-whether or not it can be moved,
+//		and ai-whether or not its a entity controlled by ai
 Entity::Entity()
 {
-	x = 0;
-	y = 0;
-	moveable = false;
-	ai = false;
+	this->x = 0;
+	this->y = 0;
+	this->moveable = false;
+	this->ai = false;
 }
+Entity::Entity(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+	this->moveable = true;
+	this->ai = false;
+}
+Entity::Entity(int x, int y, bool moveable, bool ai)
+{
+	this->x = x;
+	this->y = y;
+	this->moveable;
+	this->ai = ai;
+}
+//TODO - implement other functions inside of Entity class
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Hello this project is not done yet, so it may not work properly. You probably should not try to use it it is probably broken.
+Hello this project is not done yet, so it may not work properly. 
+You probably should not try to use it it is probably broken.
+
+
+CHANGELOG:
+
+11/9/17
+-Added Entity class and all of its method signatures
+-Implemented Constructors for Entity class
