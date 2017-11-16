a8d21ea37c7e94e4d333bf434f0bf152e97ea761
Davis Jordan <davisridleyjordan@gmail.com>
2017-11-16 11:15:29 -0500

    Added better documentation in Player

--- Player.h
+++ Player.h
@@ -4,11 +4,22 @@
 using namespace std;
 
 //class representing the ship that the player controls
+//This is a subclass of Entity so it has the variables that entity has along with 
+//others that are seen here
 class Player:Entity
 {
+//The player ship has a character that will be used to represent the ship on the sreen 
+//since images can not be used in a terminal
+//The player also has a moveRate which determines how much the player can move in one second
 private:
-	//what the ship will be represented as on the screen
 	const char * visual = "A";
-	//how many times per second the ship can move
 	const int moveRate = 5;	
+
+/*TODO:
+	-Implement constructors
+	-Implement accessors
+	-Implement function that takes input from keyboard and calls a certain move
+	function from entity depending on what key is pressed
+	-Add a function that fires a laser when the space bar is pressed
+*/
 };
