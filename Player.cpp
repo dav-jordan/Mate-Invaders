e6ca6bb429abbdc2df605ac1c0516d5806e4563a
Davis Jordan <davisridleyjordan@gmail.com>
2017-11-29 13:18:26 -0500

    Documentation

--- Player.cpp
+++ Player.cpp
@@ -4,19 +4,23 @@
 
 using namespace std;
 
+//initializes Player... For Player, ai will always be false
 Player::Player(int x, int y, bool moveable, bool ai):Entity(x, y, moveable, ai)
 {}
 
+//@return character that is used to represent the player in the gamespace
 const char * Player::getVisual() const
 {
 	return visual;
 }
 
+//@return move rate of Player
 const int Player::getMoveRate() const
 {
 	return moveRate;
 }
 
 //TODO: Implement getInput
+//takes input from keyboard and acts accordingly
 void getInput()
 {}
