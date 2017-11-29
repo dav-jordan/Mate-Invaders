fc6770587905e03f6ce7237434369f23d456c4a9
Davis Jordan <davisridleyjordan@gmail.com>
2017-11-29 13:10:22 -0500

    Implemented Constructor and accessors

--- Player.cpp
+++ Player.cpp
@@ -7,3 +7,16 @@ using namespace std;
 Player::Player(int x, int y, bool moveable, bool ai):Entity(x, y, moveable, ai)
 {}
 
+const char * Player::getVisual()
+{
+	return visual;
+}
+
+const int Player::getMoveRate()
+{
+	return moveRate;
+}
+
+//TODO: Implement getInput
+void getInput()
+{}
