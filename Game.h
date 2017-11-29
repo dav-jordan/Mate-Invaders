a8e7a435a129447b8ad5af3d4ac453224c4101b1
Davis Jordan <davisridleyjordan@gmail.com>
2017-11-29 14:40:14 -0500

    Added printGame method

--- Game.h
+++ Game.h
@@ -4,8 +4,16 @@ using namespace std;
 
 namespace game
 {
+	//@param- array to print 
+	//prints gamespace array
+	void printGame(char** space)
+	{
+		cout << "Printing gamespace" << endl;
+	}
+	
+	//loop that keeps going until the game ends
 	void gameLoop()
 	{
 		cout << "Running game loop" << endl;
-	}
+	}	
 };
