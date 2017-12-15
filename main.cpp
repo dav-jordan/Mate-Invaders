6af5706d34c2f8f470edbcce69f7b2760457ff01
dav-jordan <davisridleyjordan@gmail.com>
2017-12-14 20:07:54 -0600

    Added declaration of entity vector to main.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
 #include "Entity.h"
 #include "Player.h"
 #include "Game.h"
@@ -10,12 +10,16 @@ using namespace game;
 
 #define WIDTH 50
 #define HEIGHT 50
+
 int main()
 {
 	//to indicate the file has been excecuted
 	cout << "File successfully ran!" << endl;
 	//2d array to represent positions on the terminal screen
 	char gamespace[WIDTH][HEIGHT];
+
+    //vector that will store all entities
+    vector<Entity*> entities;
 
 	//initialize the gamespace array
 	for(int i = 0; i < HEIGHT; i++)
@@ -27,6 +31,7 @@ int main()
 	}
     //
     Player *player = new Player(0, WIDTH/2, true, false, 'a');
+    entities.push_back(player);
 	playerLoop(player, gamespace, WIDTH, HEIGHT);
 	exit(1);
 	
